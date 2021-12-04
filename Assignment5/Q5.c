0db19d32b757b87e34536a8c3668d268350418eb
chaman-rawat <chamanrawat.dev@gmail.com>
2021-12-04 12:23:36 +0530

    Updated Q5.c - Fixed Output pattern - Works for both even and odd numbers

--- Assignment5/Q5.c
+++ Assignment5/Q5.c
@@ -1,33 +1,54 @@
 // Print the following pattern on the screen (using for loop).
-// *****
-//  ***
-//   *
-//  ***
-// *****
+//  * * * *
+//    * *
+//     *
+//    * *
+//  * * * *
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
     int num = 5;
 
-    for (int row = 1; row <= num; row++)
+    int is_even = !(num & 1);
+    int middle = (num + 1) / 2;
+    int lines = (is_even) ? num - 1 : num;
+
+    for (int row = 1; row <= lines; row++)
     {
-        if (row <= (num + 1) / 2)
+        if (row <= middle) // Upper half (inclusive)
         {
-            for (int space = 1; space < row ; space++)
-                printf(" ");
-            for (int star = 1; star <= (num - 2 * (row - 1)); star++)
-                printf("*");
+            for (int space = 1; space < row; space++)
+                printf("  ");
+
+            if (row == middle && !is_even)
+                printf("\b");
+
+            for (int star = 1; star <= (num - 2 * row + 1); star++)
+                printf("* ");
+
+            if (row == middle && is_even)
+                printf("\b\b");
+
+            if (row == middle)
+                printf("* ");
         }
-        else
+        else // Lower half
         {
             for (int space = 1; space < (num - row + 1); space++)
-                printf(" ");
-            for (int star = 1; star <= 2 * (row - (num / 2)) - 1; star++)
-                printf("*");
-        }
+                printf("  ");
 
+            if (is_even)
+                printf("\b\b");
+
+            for (int star = 1; star <= 2 * (row - middle); star++)
+                printf("* ");
+
+            if (is_even)
+                printf("* ");
+        }
         printf("\n");
     }
+
     return 0;
 }
